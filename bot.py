b43b40f02d94501a7e1aea706b818978207f0704
Ojas Singh <ojas856@gmail.com>
2023-01-28 17:27:19 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import requests, json, time
+from datetime import datetime
+
+TOKEN = '5880392407:AAEoROv6_KqRzT7XKGSFb40cNDzRVAxV8L4'
+
+url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+def get_temperature():
+    weather_api_key = '46a79520f7d3d770c0900eb851944070'
+    city = 'Delhi'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    temperature = data['main']['temp']
+    return temperature
+
+def send_temperature():
+    temperature = get_temperature()
+    chat_id = '1042166601'
+    message = f'The current temperature in Delhi is {temperature} degrees Celsius'
+    data = {'chat_id': chat_id, 'text': message}
+    response = requests.post(url, json=data)
+    print(response.json())
+
+while True:
+    send_temperature()
+    time.sleep(3600)
